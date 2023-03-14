d7670f9785bb139879d79b626bbbc4134e3a7567
davidJimenezSantana <davidjimenez199701@gmail.com>
2023-03-13 23:03:51 -0500

    -Ajuste a estructura de proveedor
    -Eliminación de entidad, Dao, servicio y controlador de Telefonoroveedor

--- metalideastec/src/main/java/com/metalideas/metalideastec/persistencia/Repo/TelefonoProveedorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.metalideas.metalideastec.persistencia.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.metalideas.metalideastec.entity.TelefonoProveedor;
-
-public interface TelefonoProveedorDAO extends JpaRepository<TelefonoProveedor, Integer> {
-    
-}
